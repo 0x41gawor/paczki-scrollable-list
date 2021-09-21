8487742f1be2cba1a46369b01a7d3540904b3acd
0x41gawor <41gawor@gmail.com>
2021-09-22 00:42:27 +0200

    feat: implement ItemAdapter

--- app/src/main/java/pl/gawor/android/tutorials/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/pl/gawor/android/tutorials/affirmations/adapter/ItemAdapter.kt
@@ -1,6 +1,7 @@
 package pl.gawor.android.tutorials.affirmations.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,22 +10,39 @@ import pl.gawor.android.tutorials.affirmations.R
 import pl.gawor.android.tutorials.affirmations.model.Affirmation
 
 
+// Kotlin talks
+// class A( // members specified in brackets (stands for default constructor) : extendedClass<Generic>() // constructor call in extends list
 class ItemAdapter(private val dataset: List<Affirmation>, private val context: Context) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
     }
 
+    /**
+     * Create new views (invoked by the layout manager)
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        TODO("Not yet implemented")
+
+        val layoutInflater = LayoutInflater.from(parent.context)
+        // create a new view
+        val adapterLayout = layoutInflater.inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
     }
 
+    /**
+     * Replace the contents of a view (invoked by the layout manager)
+     */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResId)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    /**
+     * Return the size of your dataset (invoked by the layout manager)
+     */
+    // Kotlin talks
+    // If Kotlin function returns single line u can simplify it
+    override fun getItemCount() = dataset.size
 
 }
