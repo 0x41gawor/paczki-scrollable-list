cdb7285703e545595f82dbb644e8ac637ebc40de
0x41gawor <41gawor@gmail.com>
2021-09-21 23:04:23 +0200

    Display list size

--- app/src/main/java/pl/gawor/android/tutorials/affirmations/MainActivity.kt
+++ app/src/main/java/pl/gawor/android/tutorials/affirmations/MainActivity.kt
@@ -2,10 +2,18 @@ package pl.gawor.android.tutorials.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import pl.gawor.android.tutorials.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // in Kotlin we do not have `new` keyword
+        // in Kotlin we can call a method on a constructor
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
