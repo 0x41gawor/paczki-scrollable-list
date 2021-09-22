13b8f125d20a04926fa66e249aa34f2b1ada3855
0x41gawor <41gawor@gmail.com>
2021-09-22 21:57:04 +0200

    feat: Add images to data class

--- app/src/main/java/pl/gawor/android/tutorials/affirmations/model/Affirmation.kt
+++ app/src/main/java/pl/gawor/android/tutorials/affirmations/model/Affirmation.kt
@@ -1,4 +1,17 @@
 package pl.gawor.android.tutorials.affirmations.model
 
-data class Affirmation(val stringResId: Int) {
-}
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResId: Int,
+    @DrawableRes val imageResId: Int)
+
+/*
+Both stringResourceId and imageResourceId are integer values.
+Although this looks okay, the caller could accidentally pass in the arguments in the wrong order (imageResourceId first instead of stringResourceId).
+
+To avoid this, you can use Resource annotations. Annotations are useful because they add additional info to classes, methods, or parameters.
+
+Then you will get a warning if you supply the wrong type of resource ID.
+ */
